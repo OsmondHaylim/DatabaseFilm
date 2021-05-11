762190e458ccc70a38147cabf7d317dac3db0629
unknown <osmond20001@mail.unpad.ac.id>
2021-05-11 09:57:11 +0700

    Progress

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <string>
 
 #include "watchlist.hpp"
 #include "listfilm.hpp"
+
+int main(){
+    pfilm head, list;
+    std::string a;
+    head = nullptr;
+    insertFilm(list, head, a);
+    insertFilm(list, head, a);
+    insertFilm(list, head, a);
+    showListedFilm(head);
+    showsortedFilm(head);
+    showListedFilm(head);
+}
